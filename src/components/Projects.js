a16e7358789b37839d7331757900e35842780329
PranithaGandham <gpranitha2003@gmail.com>
2025-03-10 16:24:59 +0530

    My Portfolio

---
+++ src/components/Projects.js
@@ -0,0 +1,109 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Fake Currency Detection",
+      description: "ML & OpenCv",
+      imgUrl: projImg1,
+      
+    },
+    {
+      title: "Connect 4",
+      description: "Min-Max Algorithm",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Meal Bridge",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+ 
+  ];
+  // const projects2 = [
+  //   {
+  //     title: "e-Commerce",
+  //     description: "Design & Development",
+  //     imgUrl: projImg1,
+  //   },
+  //   {
+  //     title: "",
+  //     description: "ML & OpenCv",
+  //     imgUrl: projImg1,
+  //   },
+    
+  // ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>"I have developed secure and scalable backend solutions, integrating APIs, authentication, and database management. My projects emphasize performance, reliability, and real-world impact."</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    {/* <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row> */}
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>"Planning to build a feature-rich blogging website focused on seamless content management and user engagement. It will include secure authentication, a powerful backend, and an intuitive user experience."</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
